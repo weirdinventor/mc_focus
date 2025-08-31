f9b36b218be14aee9b16bb6dfabc4d85faa3c29e
root <root@srv985776.hstgr.cloud>
2025-08-31 22:39:01 +0000

    pushing from prod

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const DEV_URL = 'https://moulaclub-app-1.onrender.com/';
 
 //const DEV_URL = 'http://localhost:3000/';
-const DEV_URL = 'http://31.97.154.242:3000/';
+const DEV_URL = 'https://focus-academie.com/';
 
 export const devConfig: ConfigurationProps = {
   httpClient: axios.create({ baseURL: DEV_URL }),
--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const PROD_URL = 'https://moulaclub-app-1.onrender.com/';
 
 //const PROD_URL = 'http://localhost:3000/';
-const PROD_URL = 'http://31.97.154.242:3000/';
+const PROD_URL = 'https://focus-academie.com/';
 
 export const prodConfig: ConfigurationProps = {
   httpClient: axios.create({ baseURL: PROD_URL }),
