343ea47bb7445886ed2aeff0e0ef38d804d4d5b1
Brahim Houache <houacheibrahim@gmail.com>
2025-08-31 17:22:40 +0100

    shit 4

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -2,7 +2,7 @@ import { ConfigurationProps } from './../core/build';
 import axios from 'axios';
 
 // const DEV_URL = 'https://moulaclub-app-1.onrender.com/';
-const DEV_URL = 'http://localhost:3000/';
+const DEV_URL = 'https://mc-backend-3hau.onrender.com/';
 
 export const devConfig: ConfigurationProps = {
   httpClient: axios.create({ baseURL: DEV_URL }),
--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -2,7 +2,7 @@ import { ConfigurationProps } from './../core/build';
 import axios from 'axios';
 
 // const PROD_URL = 'https://moulaclub-app-1.onrender.com/';
-const PROD_URL = 'http://localhost:3000/';
+const PROD_URL = 'https://mc-backend-3hau.onrender.com/';
 
 export const prodConfig: ConfigurationProps = {
   httpClient: axios.create({ baseURL: PROD_URL }),
