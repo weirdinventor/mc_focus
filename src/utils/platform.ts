937496ee8fb6bc151e482e8c104f7ab22d10ba05
Brahim Houache <houacheibrahim@gmail.com>
2025-09-02 19:05:49 +0100

    update is_dev

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -1,4 +1,4 @@
  
 // export const IS_IOS = Platform.OS === 'ios';
 // export const IS_ANDROID = Platform.OS === 'android';
-export const IS_DEV =  process.env.NODE_ENV;
+export const IS_DEV = import.meta.env.VITE_IS_DEV === "true";
